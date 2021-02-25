25acb014c32aa855d6b3749afc11a295bb4095bd
David <dldudals0728@naver.com>
2021-02-25 16:06:53 +0900

    operate automation program ver 0.3.1 upload add chacking task window add progressbar(not work)

--- operate_automation_program.py
+++ operate_automation_program.py
@@ -10,28 +10,30 @@ from tkinter import filedialog
 import tkinter.ttk as ttk # combobox, progressbar
 import tkinter.messagebox as msgbox
 
-from operate_automation_menu import automation
-
 import cafe_update_module
+
+import operate_data
+
+from operate_automation_menu import automation
 #                                       example
 # preform = automation()
 # perform.auto_move_class(4, "야간")
 # perform.auto_move_report()
-# perform.automation_task_students(3, "주간", "자격증 발급,재발급 신청서")
+# perform.automation_task_students(3, "주간", "요양보호사 자격증 발급,재발급 신청서")
 # perform.automation_task_report(5, "주간", "개강보고") # 수료보고가 따로 없기 때문에 kind = 개강보고로 고정 !
 # perform.automation_task_temporary(4, "주간", "수료보고")
 # perform.mkfile("5기주간0201", "교육수료증명서.hwp") # 뒤에 복사할 파일을 입력할 때 꼭 !!!!! 확장자 명까지 작성하기 ㅎㅎ
 # perform.mkattendance(3, "야간")
 
 
-global frame_run
 global radio_num_var
 global radio_time_var
 global frame_option
 global cmbbox
 
+function = automation()
+
 def automation_start():
-    get_start = automation()
     ordinal_num = radio_num_var.get()
     time = radio_time_var.get()
     mode = cmbbox.get()
@@ -39,31 +41,31 @@ def automation_start():
     print("time :", time, "time type", type(time))
     print("mode :", mode, "mode type :", type(mode))
     mode_baseinfo = ["출석시간 반영", "교육생 자료 복사"]
-    mode_manage = ["교육수료증명서", "대체실습확인서", "자격증 발급,재발급 신청서"]
+    mode_manage = ["교육수료증명서", "대체실습확인서", "요양보호사 자격증 발급,재발급 신청서"]
     mode_report = ["개강보고", "출석부", "실시보고_대체실습", "수료보고_대체실습", "출석부_대체실습"]
     # mode list : "교육수료증명서", "대체실습확인서", "요양보호사 자격증 발급 신청서", "출석시간 반영", "교육생 자료 복사", "개강보고(실시보고)", "종강보고(수료보고)", "출석부", "대체실습"
 
     if mode == mode_baseinfo[0]:
-        get_start.update_attendance(ordinal_num, time)
+        function.update_attendance(ordinal_num, time)
 
     # elif mode == mode_baseinfo[1]:
-        # get_start.mkfile(ordinal_num, time, )
+        # function.mkfile(ordinal_num, time, )
 
     if mode in mode_manage:
-        get_start.auto_move_class(ordinal_num, time)
-        get_start.automation_task_students(ordinal_num, time, mode)
+        function.auto_move_class(ordinal_num, time)
+        function.automation_task_students(ordinal_num, time, mode, modevar)
 
     if mode in mode_report:
         if mode == mode_report[0]:
-            get_start.automation_task_report(ordinal_num, time, mode)
+            function.automation_task_report(ordinal_num, time, mode)
         elif mode == mode_report[1]:
-            get_start.mkattendance(ordinal_num, time)
+            function.mkattendance(ordinal_num, time)
         elif mode == mode_report[2]:
             mode = mode_report[2][:4]
-            get_start.automation_task_temporary(ordinal_num, mode)
+            function.automation_task_temporary(ordinal_num, mode)
         elif mode == mode_report[3]:
             mode = mode_report[3][:4]
-            get_start.automation_task_temporary(ordinal_num, mode)
+            function.automation_task_temporary(ordinal_num, mode)
         elif mode == mode_report[4]:
             msgbox.showerror("업데이트 필요", "대체실습 출석부 작성 자동화 프로그램이 업데이트 되지 않았습니다.(업데이트 필요) ")
 
@@ -74,27 +76,19 @@ def del_widget(user_widget):
     if user_widget.winfo_exists():
         user_widget.destroy()
 
-#####################################################################################################
-# def mk_progressbar(user_widget, total_point):
-#     p_var = DoubleVar()
-#     frame_p = Frame(user_widget)
-#     frame_p.pack(fill="x")
-#     progressbar = ttk.Progressbar(frame_p, maximum=100, variable=p_var)
-#     progressbar.pack(fill="x")
-#     def update():
-#         point = i / total_point * 100
-#         p_var.set(point)
-#         progressbar.update()
-        
-
-#####################################################################################################
+def add_listbox(listbox):
+    group = f"{radio_num_var.get()}기{radio_time_var.get()}"
+    for idx, cell in enumerate(function.ws_members["E"], start=1):
+        if not group in str(cell.value):
+            continue
+        string = f"{function.ws_members.cell(row=idx, column=1).value}. {function.ws_members.cell(row=idx, column=18).value}"
+        listbox.insert(END, string)
+    listbox.config(state=DISABLED)
 
 def selection():
-    global frame_run
-    global lstbox_members
-
     if cmbbox.get() == "선택":
         msgbox.showinfo("알림", "자동화 옵션을 선택해주세요.")
+        return
     else:
         ready = msgbox.askyesno("선택", f"{radio_num_var.get()}기 {radio_time_var.get()}반 {cmbbox.get()} 자동화 작업을 선택하셨습니다.")
         print(ready)
@@ -106,37 +100,33 @@ def selection():
             msgbox.showinfo("알림", "자동화 옵션을 다시 선택해주세요.")
             return
 
-    lst_selection = ["선택", "교육수료증명서", "대체실습확인서", "자격증 발급,재발급 신청서"]
+    lst_selection = ["선택", "교육수료증명서", "대체실습확인서", "요양보호사 자격증 발급,재발급 신청서"]
     lst_selection.remove("선택")
-    # frame_run = Frame(frame_2) # 이걸 안하면 계속 빨간줄 생김 ,, / 근데 이걸 하면 선택을 두번 연속 눌렀을 때 프레임이 안사라짐 ,,
-    del_widget(frame_run)
 
-    # if cmbbox.get() in lst_selection:
-    frame_run = Frame(frame_2)
-    frame_run.pack(fill="x")
+    lst_baseinfo = ["출석시간 반영", "교육생 자료 복사"]
+    lst_manage = ["교육수료증명서", "대체실습확인서", "요양보호사 자격증 발급,재발급 신청서"]
+    lst_report = ["개강보고", "출석부", "실시보고_대체실습", "수료보고_대체실습", "출석부_대체실습"]
 
-    frame_members = Frame(frame_run)
-    frame_members.pack(fill="x")
+    group = f"{radio_num_var.get()}기{radio_time_var.get()}"
+    print(group)
+    members = 0
 
-        # scrbar = Scrollbar(frame_members)
-        # scrbar.pack(side="right", fill="y")
+    listbox.config(state=NORMAL)
+    listbox.delete(0, END)
 
-        # lstbox_members = Listbox(frame_members, selectmode="extended", height=10, yscrollcommand=scrbar.set)
-        # lstbox_members.pack(side="left", fill="both", expand=True)
+    if cmbbox.get() == "출석시간 반영":
+        info_message = "D:\\"+operate_data.ac_name+"\\교육생관리\\출석부_기관장용\n폴더 내부의 파일들을 참조하여 출석부를 최신화 합니다."
+    elif cmbbox.get() == "교육생 자료 복사":
+        info_message = "D:\\Master\\mkfile\n폴더 내부의 파일들을 참조하여 출석부를 최신화 합니다."
+    elif cmbbox.get() in lst_manage or cmbbox.get() in lst_report:
+        info_message = "작성된 명단의 내용을 참조하여 \"" + cmbbox.get() + "\" 작성을 시작합니다."
+    
+    listbox.insert(END, info_message)
+    add_listbox(listbox)
+    
+        
 
-        # for i in range(1, 31):
-        #     lstbox_members.insert(END, str(i) + "번")
-
-        # scrbar.config(command=lstbox_members.yview)
-
-    frame_btn_run = Frame(frame_run)
-    frame_btn_run.pack(fill="x")
-
-    btn_run_satrt = Button(frame_btn_run, text="실행", width=12, command=automation_start)
-    btn_run_satrt.pack(side="right", padx=5, pady=5)
-
-
-
+    
 def basic():
     global radio_num_var
     global radio_time_var
@@ -190,13 +180,21 @@ def baseinfo():
     cmbbox["values"] = lst
     cmbbox.current(0)
 
+    radio_xlsx.select()
+    radio_xlsx["state"] = "disable"
+    radio_hwp["state"] = "disable"
+
 def manage():
     global frame_option
     global cmbbox
 
-    lst = ["선택", "교육수료증명서", "대체실습확인서", "자격증 발급,재발급 신청서"]
+    lst = ["선택", "교육수료증명서", "대체실습확인서", "요양보호사 자격증 발급,재발급 신청서"]
     cmbbox["values"] = lst
     cmbbox.current(0)
+
+    radio_xlsx.select()
+    radio_xlsx["state"] = "normal"
+    radio_hwp["state"] = "normal"
 
 def report():
     global frame_option
@@ -206,9 +204,13 @@ def report():
     lst = ["선택", "개강보고", "출석부", "실시보고_대체실습", "수료보고_대체실습", "출석부_대체실습"]
     cmbbox["values"] = lst
     cmbbox.current(0)
+
+    radio_hwp.select()
+    radio_xlsx["state"] = "disable"
+    radio_hwp["state"] = "disable"
         
 def test():
-    pass
+    print(modevar.get())
 
 def check_update():
     update_window = Tk()
@@ -228,10 +230,13 @@ def check_update():
     label2.grid(row=2, column=0, sticky=W)
 
     label3 = Label(frame, text="ver. 0.1.2   옵션선택 위치 조정   21.02.16\n\n")
-    label3.grid(row=3, column=0)
+    label3.grid(row=3, column=0, sticky=W)
 
     label4 = Label(frame, text="ver. 0.2.1   combobox 동적 값 할당   21.02.16\n\n")
-    label4.grid(row=4, column=0)
+    label4.grid(row=4, column=0, sticky=W)
+
+    label5 = Label(frame, text="ver. 0.3.1   진행 업무 확인, progressbar 추가   21.02.25\n\n")
+    label5.grid(row=5, column=0, sticky=W)
 
     update_window.mainloop()
 
@@ -241,8 +246,14 @@ def start():
 
     print(cmbbox.get())
 
+    for i in range(101):
+        pvar.set(i)
+        progressbar.update()
+        pyautogui.sleep(0.01)
+
 def cafe_update():
     cafe_update_module.cafe_update()
+
 
 root = Tk()
 root.title("교육원 운영 자동화 프로그램")
@@ -268,33 +279,57 @@ btn_report.pack(side="left", padx=10, pady=5)
 btn_test.pack(side="left", padx=10, pady=5)
 
 # 1번 프레임
-frame_1 = Frame(main_frame)
+frame_1 = Frame(main_frame, bd=1, relief="solid")
 frame_1.pack(fill="x")
-label_1 = Label(frame_1, text=" ")
-label_1.pack(side="bottom")
 
 frame_option = LabelFrame(frame_1, text="업무 자동화")
 frame_option.pack(fill="both")
 
+# Option 선택 combobox frame
 frame_cmbbox = Frame(frame_option)
 frame_cmbbox.pack()
 label = Label(frame_cmbbox, text="Option")
 label.pack(side="left", padx=5, pady=5)
 
 lst = ["선택"]
-cmbbox = ttk.Combobox(frame_cmbbox, state="readonly", values=lst)
+cmbbox = ttk.Combobox(frame_cmbbox, state="readonly", values=lst, width=30)
 cmbbox.current(0)
 cmbbox.pack(side="left", padx=5, pady=5)
 
+# excel version frame
+frame_mode = Frame(frame_cmbbox)
+frame_mode.pack(side="right")
+
+label_mode = Label(frame_mode, text="mode selection")
+label_mode.pack(side="top")
+
+frame_mode_select = Frame(frame_mode)
+frame_mode_select.pack(side="bottom")
+
+modevar = BooleanVar()
+radio_xlsx = Radiobutton(frame_mode_select, text="Excel", value=True, variable=modevar)
+radio_xlsx.select()
+radio_hwp = Radiobutton(frame_mode_select, text="한글", value=False, variable=modevar)
+radio_xlsx.pack(side="left", padx=5, pady=5)
+radio_hwp.pack(side="right", padx=5, pady=5)
 basic()
 
 # 2번 프레임
 frame_2 = Frame(main_frame)
 frame_2.pack(fill="both")
 
-# 프로그램 실행 프레임
-frame_run = Frame(frame_2)
-frame_run.pack(fill="x")
+# 진행상황 프레임
+frame_progress = Frame(frame_2)
+frame_progress.pack(fill="both")
+frame_list = Frame(frame_progress)
+frame_list.pack(fill="both")
+scrollbar = Scrollbar(frame_list)
+scrollbar.pack(side="right", fill="y")
+listbox = Listbox(frame_list, yscrollcommand=scrollbar.set, disabledforeground="black")
+listbox.pack(fill="both")
+pvar = DoubleVar()
+progressbar = ttk.Progressbar(frame_progress, maximum=100, variable=pvar)
+progressbar.pack(side="bottom", fill="both")
 
 # 서브 프레임
 sub_frame = Frame(root, relief="solid", bd=1)
@@ -302,7 +337,7 @@ sub_frame.pack(side="right", padx=5, pady=5, fill="both", expand=True)
 
 # side = bottom
 # ver 0.1.1 : 프레임 순서 설정  / 0.1.2 : basic 함수에 프레임 추가로 가독성 상승
-label_verinfo = Label(sub_frame, text="버전정보 0.2.1")
+label_verinfo = Label(sub_frame, text="버전정보 0.3.1")
 label_verinfo.pack(side="bottom")
 
 btn_update = Button(sub_frame, text="업데이트 내역", command=check_update, width=12)
@@ -319,7 +354,7 @@ btn_quit.pack(padx=5, pady=5)
 
 
 
-
+scrollbar.config(command=listbox.yview)
 root.mainloop()
 
 a = pyautogui.getWindowsWithTitle("교육원 운영 자동화 프로그램")[0]
